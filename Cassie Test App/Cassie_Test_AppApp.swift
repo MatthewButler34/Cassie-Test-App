251f593026d4388a82a58412ee1d92e87889eff1
Matthew Butler <Mattjbutler2000@gmail.com>
2023-09-04 11:31:17 -0500

    Initial Commit

---
+++ Cassie Test App/Cassie_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cassie_Test_AppApp.swift
+//  Cassie Test App
+//
+//  Created by Matthew Butler on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Cassie_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
